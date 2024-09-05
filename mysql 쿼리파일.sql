de8009e47061cd77f1391de0dd0e30261f07900d
gabintree <shinhwa0718@nate.com>
2024-09-05 17:38:14 +0900

    mysql 쿼리파일, db 정보, gitignore파일 update

--- mysql 쿼리파일.sql
+++ mysql 쿼리파일.sql
@@ -6,13 +6,13 @@
 -- age int,
 -- phone varchar(30),
 -- user_gbn varchar(1), -- 0 : 사용자 계정, 1 : 관리자(병원) 계정
--- remark varchar(100),
+-- hospital_id varchar(30), -- uer_gbn이 1일 경우 기관ID 입력 필수, 기본값 null
 -- insert_date datetime, -- 가입일
 -- update_date datetime, -- 수정일
 -- primary key(id)
 -- );
 
--- insert into user (id, password, name, gender, age, phone, user_gbn, remark, insert_date, update_date) 
--- 		  values ('admin0', '0000', '사용자측', '0', 30, '010-1234-1234', '0', '사용자계정으로 접속', now(), now() ) 
+-- insert into user (id, password, name, gender, age, phone, user_gbn, hospital_id, insert_date, update_date) 
+-- 		  values ('admin0', '0000', '사용자측', '0', 30, '010-1234-1234', '0', null, now(), now() ) ;
 
 select * from user;
