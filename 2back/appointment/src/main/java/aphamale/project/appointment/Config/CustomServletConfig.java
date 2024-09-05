6e76cfbd0e0af19fb9fb66ffc7434eaa58fbf844
gabintree <shinhwa0718@nate.com>
2024-09-05 09:43:31 +0900

    프로젝트 환경설정

---
+++ 2back/appointment/src/main/java/aphamale/project/appointment/Config/CustomServletConfig.java
@@ -0,0 +1,19 @@
+package aphamale.project.appointment.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomServletConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("HEAD","GET","POST","PUT","DELETE","OPTIONS")
+                .maxAge(3000)
+                .allowedHeaders("Authorization","Cache-Control","Content-Type");
+    }
+    
+}
