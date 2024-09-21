b8a2728015c3ed2a10d48c895f9724fe34bf166f
gabintree <shinhwa0718@nate.com>
2024-09-21 11:49:07 +0900

    gender radio 단일 선택 기능, 입력 컬컬럼 추가

--- 1front/appointment/src/pages/Register.js
+++ 1front/appointment/src/pages/Register.js
@@ -1,4 +1,4 @@
-import { Card, Form, Button, Nav, Alert } from "react-bootstrap";
+import { Card, Form, Button, Nav, Alert, FormCheck } from "react-bootstrap";
 import React from "react";
 import { useState } from "react";
 import { json, useNavigate } from "react-router-dom"; // useRouter 대신 useNavigate 사용
@@ -10,9 +10,18 @@ const Register = () => {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
+  const [name, setName] = useState("");
+  const [regNo1, setRegNo1] = useState("");
+  const [regNo2, setRegNo2] = useState("");
+  const [birthday, setBirthday] = useState(""); // 생년월일  6자리
+  const [gender, setGender] = useState("");
+  const [phone, setPhone] = useState("");
+
   const [tab, setTab] = useState("user"); // 탭 상태 추가
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [ischecked0, setIschecked0] = useState();
+  const [ischecked1, setIschecked1] = useState();
 
   // TODO: 관리자 버튼을 클릭할 때 사용자 버튼이 동시에 눌려지는 문제 해결
   // TODO: 관리자 회원가입에 회원정보 입력하고 제출하면 사용자 대시보드로 이동함
@@ -42,7 +51,12 @@ const Register = () => {
 
   const data = {
     "userId" : user,
-    "userPw" : password
+    "userPw" : password,
+    "userName" : name,
+    "residentNo" : regNo1 + '-' + regNo2,
+    "birthDate" : birthday,
+    "gender" : gender,
+    "phone" : phone
   }
 
   const postJoinData = async () => {
@@ -66,6 +80,20 @@ const Register = () => {
     }
   }
 
+  function genderOnChange(selectGender) {
+    console.log("gender값 : " , selectGender);
+    if (selectGender == 0){
+      setIschecked0(true);
+      setIschecked1(false);
+      setGender(selectGender);
+    }
+    else{
+      setIschecked0(false);
+      setIschecked1(true);
+      setGender(selectGender);
+    }
+  } 
+ 
   return (
     <Card>
       <h1>Logo</h1>
@@ -115,6 +143,67 @@ const Register = () => {
               onChange={(e) => setPassword2(e.target.value)}
             />
           </Form.Group>
+          <Form.Group controlId="formUsername2">
+            <Form.Label>이름</Form.Label>
+            <Form.Control
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formUserRegNo">
+            <Form.Label>주민등록번호</Form.Label>
+            <Form.Control
+              type="text"
+              value={regNo1}
+              onChange={(e) => setRegNo1(e.target.value)}
+            />
+            <Form.Control
+              type="text"
+              value="-"
+              disabled={true}          
+            />
+            <Form.Control
+              type="text"
+              value={regNo2}
+              onChange={(e) => setRegNo2(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formUserBirthDay">
+            <Form.Label>생년월일</Form.Label>
+            <Form.Control
+              type="text"
+              value={birthday}
+              onChange={(e) => setBirthday(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formUserGender">
+            <Form.Label>성별 </Form.Label>
+            <Form.Label>남성 </Form.Label>
+            <Form.Control 
+              type="radio"
+              value={0}
+              checked={ischecked0}
+              defaultChecked={true}
+              onClick={(e) => genderOnChange(e.target.value)} 
+            />
+            <Form.Label>여성 </Form.Label>
+            <Form.Control
+              type="radio"
+              value={1}
+              checked={ischecked1}
+              defaultChecked={false}
+              onClick={(e) => genderOnChange(e.target.value)}              
+            />                     
+          </Form.Group>                     
+          <Form.Group controlId="formUserPhone">
+            <Form.Label>연락처</Form.Label>
+            <Form.Control
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </Form.Group>        
           {error && (
             <div className="mt-3">
               <Alert variant="danger">{error}</Alert>
