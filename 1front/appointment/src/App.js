89d2407ddc41ec48ca38f34211b1b43bb60bf08f
balbari <anstjswk50@naver.com>
2024-09-24 16:45:05 +0900

    파일

--- 1front/appointment/src/App.js
+++ 1front/appointment/src/App.js
@@ -1,56 +1,92 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom"; // Router 사용때 에러발생 -> BrowserRouter 대체
+import axios from "axios";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserDashboard from "./pages/UserDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import Dashboard from './pages/HDashBoard';
+import ReservationManagement from './pages/ReservationManagement';
+import StatusAndDetails from './pages/StatusAndDetails.js';
 
-// export default App;
-
-
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 
 function App() {
-   const [hello, setHello] = useState('')
-
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-            {/* <a href='/api/save'>회원가입 </a>
-            <a href='/api/login'>로그인 </a>
-            <a href='/api/list'>목록 조회 </a> */}
-
-            <form action="api/save" method="post">
-                <input type="text" name="id" placeholder="아이디"></input>
-                <input type="text" name="password" placeholder="비밀번호"></input>
-                <input type="submit" value="회원가입"></input>
-            </form>
-        </div>
-    );
+  // NavBar 레이아웃 테스트
+  return (
+    <BrowserRouter>
+      <NavBar />
+      {/* 라우팅 설정 */}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/UserDashboard" element={<UserDashboard />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
+        <Route path="/Dashboard" element={<Dashboard />}/>
+        <Route path="/ReservationManagement" element={<ReservationManagement />} />
+        <Route path="/StatusAndDetails" element={<StatusAndDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+// const App = () => {
+//   return (
+//       <div className="app">
+//           <Dashboard />
+//           <ReservationManagement />
+//           <StatusAndDetails />
+//       </div>
+//   );
+// };
+
+
+// 초기 테스트
+// import React, {useEffect, useState} from 'react';
+// import {  Routes, Route, BrowserRouter } from "react-router-dom";
+// import axios from 'axios';
+// function App() {
+//    const [hello, setHello] = useState('')
+
+//     useEffect(() => {
+//         axios.get('/api/hello')
+//         .then(response => setHello(response.data))
+//         .catch(error => console.log(error))
+//     }, []);
+
+//     return (
+//         <div>
+//             <h1 className="text-3xl font-bold underline">
+//                 백엔드에서 가져온 데이터입니다 : {hello}
+//             </h1>
+
+//             {/* <a href='/api/save'>회원가입 </a> <br></br>
+//             <a href='/api/login'>로그인 </a> <br></br>
+//             <a href='/api/list'>목록 조회 </a> <br></br> */}
+
+//             <form action="api/save" method="post">
+//                 <input type="text" name="userId" placeholder="아이디"></input>
+//                 <input type="text" name="userPw" placeholder="비밀번호"></input>
+//                 <input type="submit" value="회원가입"></input>
+//             </form>
+            
+//             <form action="api/login" method="post">
+//                 <input type="text" name="userId" placeholder="아이디"></input>
+//                 <input type="password" name="userPw" placeholder="비밀번호"></input>
+//                 <input type="submit" value="로그인"></input>
+//             </form>    
+
+//             {/* <BrowserRouter>
+//                 <Routes>
+//                     <Route path="/" exact element={Home}/>
+//                     <Route path="/api/login" component={Login} />
+//                 </Routes>
+//             </BrowserRouter> */}
+//         </div>
+//     );
+// }
 
 export default App;
